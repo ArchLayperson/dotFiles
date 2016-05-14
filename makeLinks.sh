8d83c23314b44c00ad417c55d3a998e7c1f27c90
Aaron Haun <aaron@haun.guru>
2016-05-14 19:47:37 +0000

    Add xbindkeysrc

--- makeLinks.sh
+++ makeLinks.sh
@@ -3,4 +3,6 @@ scriptFile=$(readlink -f "$0")
 scriptDir=$(dirname "$scriptFile")
 ln -s $scriptDir/.zshrc $HOME/.zshrc
 ln -s $scriptDir/.vimrc $HOME/.vimrc
+ln -s $scriptDir/.xkeybindrc $HOME/.xkeybindrc
+
 
